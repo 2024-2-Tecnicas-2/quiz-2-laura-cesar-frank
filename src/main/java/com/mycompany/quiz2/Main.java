daa852f91f548c642cc6821d468bee76fb4a6154
Jhon doe <Jhon.doe1112@gmail.com>
2024-09-30 20:57:38 -0500

    Clase Main

---
+++ src/main/java/com/mycompany/quiz2/Main.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.quiz2;
+
+  public class Main {
+    public static void main(String[] args) {
+        Coche coche = new Coche("Toyota", "Corolla","Mazda");
+        Lancha lancha = new Lancha("Yamaha", "Ducati");
+
+        coche.acelerar();
+        coche.acelerar();
+        coche.frenar();
+        
+        lancha.acelerar();
+
+        System.out.println(coche);
+        System.out.println("Tipo de combustible del coche: " + coche.tipoCombustible());
+        
+        System.out.println(lancha);
+        System.out.println("Tipo de combustible de la lancha: " + lancha.tipoCombustible());
+    }
+}
