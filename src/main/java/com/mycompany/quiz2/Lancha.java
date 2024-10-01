67a7fa4e08aa1c6d14acd62321c10de4da7b13c8
Name <correo@gmai.com>
2024-09-30 20:47:36 -0500

    Se añade el constructor clases lancha y  coche

--- src/main/java/com/mycompany/quiz2/Lancha.java
+++ src/main/java/com/mycompany/quiz2/Lancha.java
@@ -2,4 +2,8 @@ package com.mycompany.quiz2;
 
 public class Lancha extends VehiculoBase{
     
+    public Lancha(String marca, String modelo, int velocidadActual) {
+        super(marca, modelo, velocidadActual);
+    }
+    
 }
