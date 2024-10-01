04e141c880f78c8b0c97151802e2054f1472e339
Autor <Autor@konradlorenz.edu.co>
2024-09-30 21:00:46 -0500

    se pone get y set

--- src/main/java/com/mycompany/quiz2/VehiculoBase.java
+++ src/main/java/com/mycompany/quiz2/VehiculoBase.java
@@ -9,6 +9,38 @@ abstract class VehiculoBase implements Vehiculo{
           private int velocidadActual;
           private String TipoCombustible;
 
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getVelocidadActual() {
+        return velocidadActual;
+    }
+
+    public void setVelocidadActual(int velocidadActual) {
+        this.velocidadActual = velocidadActual;
+    }
+
+    public String getTipoCombustible() {
+        return TipoCombustible;
+    }
+
+    public void setTipoCombustible(String TipoCombustible) {
+        this.TipoCombustible = TipoCombustible;
+    }
+
           
           
           
